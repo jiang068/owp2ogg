f37ee856d3bef9e8e6d14c7704303f13586a3662
jiang068 <fj666@mail.ustc.edu.cn>
2024-07-28 16:34:49 +0800

    python脚本文件
    py文件

---
+++ owp2ogg.py
@@ -0,0 +1,36 @@
+import os
+
+def convert_owp_to_ogg():
+    BUFSIZE = 1024  # 缓冲区大小
+    xor_key = 0x39  # 异或运算的密钥
+
+    input_directory = "./owp"
+    output_directory = "./ogg"
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for filename in os.listdir(input_directory):
+        if filename.endswith(".owp"):
+            filepath = os.path.join(input_directory, filename)
+            outfilename = os.path.join(output_directory, os.path.splitext(filename)[0] + ".ogg")
+            print(f"Opening... {outfilename}")
+
+            try:
+                with open(filepath, "rb") as fp, open(outfilename, "wb") as fpo:
+                    while True:
+                        buf = fp.read(BUFSIZE)
+                        if not buf:
+                            break
+                        buf = bytearray(buf)
+                        for j in range(len(buf)):
+                            buf[j] ^= xor_key
+                        fpo.write(buf)
+            except IOError as e:
+                print(f"Cannot open: {outfilename}\n{e}")
+                continue
+
+            print(f"Complete: {filename}\n")
+
+if __name__ == "__main__":
+    convert_owp_to_ogg()
